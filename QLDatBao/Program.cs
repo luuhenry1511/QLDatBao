fbf2e4c3d0395d3a1b093e7a7e32469f696cddc5
Henry <Henry@DESKTOP-RD7GGH3>
2020-06-18 21:05:47 +0700

    Delete dư thừa

--- QLDatBao/Program.cs
+++ QLDatBao/Program.cs
@@ -9,7 +9,7 @@ namespace QLDatBao
     static class Program
     {
         /// <summary>
-        /// The main entry point for the application. cccccc  nnnn
+        /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
